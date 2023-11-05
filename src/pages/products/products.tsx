61409d4036fe1e61b255b40b6e20b30452553fff
marzieh <marziehmonshizade@yahoo.com>
2023-11-05 21:07:45 +0330

    get : get product by react-query

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,39 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useQuery } from "react-query";
 import DeleteModal from "../../components/Modals/deleteModal";
 import DetailsModal from "../../components/Modals/detailsModal";
 import EditModal from "../../components/Modals/editModal";
 import AddNewProduct from "../../components/addNewProduct/addNewProduct";
-import apiRequests from "../../services/configs";
-interface products {
-  id: number;
-  name: string;
-  img: string;
-  price: string;
-  count: string;
-}
+import { Product } from "../../components/product/product";
+
 const Products = () => {
-  const [myProducts, setMyProducts] = useState<products[]>();
+  // state
   const [isShowModal, setIsShowModal] = useState<boolean>(false);
   const [isShowDetailsModal, setIsShowDetailsModal] = useState<boolean>(false);
   const [isShowEditModal, setIsShowEditModal] = useState<boolean>(false);
 
+  // funcs
   const modalCancel = () => {
     setIsShowModal(false);
   };
   const modalSubmit = () => {
     setIsShowModal(false);
   };
-  const loadProducts = async () => {
-    const res = await apiRequests.get("/products");
-    setMyProducts(res?.data);
-    console.log("res =>", myProducts);
-  };
   const editInfosSubmit = (e: any) => {
     e.preventDefault();
+    setIsShowEditModal(false);
+    console.log(isShowEditModal);
   };
-  useEffect(() => {
-    loadProducts();
-  }, []);
+  const { data, isLoading, isError } = useQuery("Products", () =>
+    fetch("http://localhost:3001/products").then((res) => res.json())
+  );
   return (
     <>
       <AddNewProduct />
@@ -48,51 +41,31 @@ const Products = () => {
               <th className="py-3">وضعیت</th>
             </tr>
           </thead>
-
+          {isLoading && (
+            <div className="mx-auto text-gray-500">لطفا کمی صبر کنید...</div>
+          )}
+          {isError && (
+            <div className="text-red-700">
+              متاسفانه مشکلی پیش آمده است نمی توانیم داده ها را نمایش دهیم
+            </div>
+          )}
           <tbody>
-            {myProducts?.map((i): JSX.Element => {
-              return (
-                //move to product component
-                <tr key={i.id}>
-                  <td className="md:flex justify-center">
-                    <img src={i.img} alt="hb" className="max-h-40" />
-                  </td>
-                  <td className="md:text-center py-8"> {i.name}</td>
-                  <td className="md:text-center py-8">
-                    {i.price.toLocaleString()} تومان
-                  </td>
-                  <td className="md:text-center py-8">{i.count}</td>
-                  <td className="py-8 text-center">
-                    <button
-                      type="button"
-                      className="btn"
-                      onClick={() => setIsShowDetailsModal(true)}
-                    >
-                      جزئیات
-                    </button>
-                    <button
-                      type="button"
-                      className="btn"
-                      onClick={() => {
-                        setIsShowModal((prev) => !prev);
-                      }}
-                    >
-                      حذف
-                    </button>
-                    <button
-                      type="button"
-                      className="btn"
-                      onClick={() => setIsShowEditModal(true)}
-                    >
-                      ویرایش
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
+            {data?.map(
+              (item: any): JSX.Element => (
+                <Product
+                  key={item.id}
+                  setIsShowModal={setIsShowModal}
+                  setIsShowDetailsModa={setIsShowDetailsModal}
+                  setIsShowEditModal={setIsShowEditModal}
+                  {...item}
+                />
+              )
+            )}
           </tbody>
         </table>
       </div>
+
+      {/* modals */}
       {isShowModal && (
         <DeleteModal submitAction={modalSubmit} cancelAction={modalCancel} />
       )}
@@ -105,12 +78,26 @@ const Products = () => {
             console.log("close");
             setIsShowEditModal(false);
           }}
-          onSubmit={() => {
-            console.log("submit");
-            editInfosSubmit;
-          }}
+          onSubmit={editInfosSubmit}
         >
-          i am children
+          <input
+            className="input mx-auto my-2 text-right font-normal"
+            type="text"
+            placeholder="نام جدید محصول را وارد کنید "
+            aria-label="Full name"
+          />
+          <input
+            className="input mx-auto my-2 text-right font-normal"
+            type="text"
+            placeholder="قیمت جدید محصول را وارد کنید "
+            aria-label="Full name"
+          />
+          <input
+            className="input mx-auto my-2 text-right font-normal"
+            type="text"
+            placeholder="تعداد جدید محصول را وارد کنید "
+            aria-label="Full name"
+          />
         </EditModal>
       )}
     </>
