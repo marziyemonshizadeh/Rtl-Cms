3d0b4ed262f005e095ef8f08d086d42548932b70
marzieh <marziehmonshizade@yahoo.com>
2023-11-06 21:51:31 +0330

    get comment on comments page

--- src/pages/comments/comments.tsx
+++ src/pages/comments/comments.tsx
@@ -1,53 +1,42 @@
+import { useQuery } from "react-query";
+import Comment from "../../components/comment/comment";
 import ErrorBox from "../../components/errorBox/errorBox";
 
 const Comments: React.FC = () => {
+  const { data, isLoading } = useQuery("comments", () =>
+    fetch("http://localhost:3001/comments").then((res) => res.json())
+  );
   return (
     <>
       <h1 className="header"> کامنت های محصولات</h1>
-      <ErrorBox message="هیچ کامنتی یافت نشد" />
-
-      <div className="card mt-5">
-        <table className="w-full">
-          <thead className="border-b">
-            <tr>
-              <th className="py-8">اسم کاربر</th>
-              <th className="py-8">محصول</th>
-              <th className="py-8">کامنت</th>
-              <th className="py-8">تاریخ</th>
-              <th className="py-8">ساعت</th>
-              <th className="py-8">وضعیت</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="text-center py-8">مرضیه منشی زاده </td>
-              <td className="text-center py-8">لپ تاپ msi</td>
-              <td className="text-center py-8">
-                {" "}
-                <button type="button" className="btn">
-                  دیدن کامنت
-                </button>{" "}
-              </td>
-              <td className="text-center py-8">13 شهریور 1402 </td>
-              <td className="text-center py-8">16:30</td>
-              <td className="py-8 text-center">
-                <button type="button" className="btn">
-                  حذف
-                </button>
-                <button type="button" className="btn">
-                  تایید
-                </button>
-                <button type="button" className="btn">
-                  پاسخ
-                </button>
-                <button type="button" className="btn">
-                  ویرایش
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {data?.length ? (
+        <div className="card mt-5">
+          <table className="w-full">
+            <thead className="border-b">
+              <tr>
+                <th className="py-8">اسم کاربر</th>
+                <th className="py-8">محصول</th>
+                <th className="py-8">کامنت</th>
+                <th className="py-8">تاریخ</th>
+                <th className="py-8">ساعت</th>
+                <th className="py-8">وضعیت</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isLoading && (
+                <div className="mx-auto text-gray-500">
+                  لطفا کمی صبر کنید...
+                </div>
+              )}
+              {data?.map((item: any) => {
+                return <Comment key={item.id} {...item} />;
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <ErrorBox message="هیچ کامنتی یافت نشد" />
+      )}
     </>
   );
 };
