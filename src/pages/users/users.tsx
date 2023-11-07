0e60a5dffdefe16369a5a9e458f7e742980eb2f9
marzieh <marziehmonshizade@yahoo.com>
2023-11-07 17:48:25 +0330

    refactor: refactor users  page

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,45 +1,40 @@
+import { useQuery } from "react-query";
 import ErrorBox from "../../components/errorBox/errorBox";
+import User from "../../components/user/user";
 
 const Users: React.FC = () => {
+  const { data, isLoading } = useQuery("users", () =>
+    fetch("http://localhost:3001/users").then((res) => res.json())
+  );
   return (
     <>
       <h1 className="header">لیست کاربران</h1>
-      <ErrorBox message="هیچ کاربری یافت نشد" />
-
-      <div className="card mt-5">
-        <table className="w-full">
-          <thead className="border-b-2">
-            <tr>
-              <th className="py-8">نام و نام خانوادگی</th>
-              <th className="py-8">نام کاربری </th>
-              <th className="py-8">رمز عبور</th>
-              <th className="py-8">شماره تماس</th>
-              <th className="py-8">ایمیل</th>
-              <th className="py-8">وضعیت</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="text-center py-8">مرضیه منشی زاده </td>
-              <td className="text-center py-8">Marziii</td>
-              <td className="text-center py-8">qaz123</td>
-              <td className="text-center py-8">0935555555</td>
-              <td className="text-center py-8">marziehmonshizade@yahoo.com</td>
-              <td className="py-8 text-center">
-                <button type="button" className="btn">
-                  حذف
-                </button>
-                <button type="button" className="btn">
-                  جزئیات
-                </button>
-                <button type="button" className="btn">
-                  ویرایش
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {isLoading && (
+        <div className="mx-auto text-gray-500">لطفا کمی صبر کنید...</div>
+      )}
+      {data?.length ? (
+        <div className="card mt-5">
+          <table className="w-full">
+            <thead className="border-b-2">
+              <tr>
+                <th className="py-8">نام و نام خانوادگی</th>
+                <th className="py-8">نام کاربری </th>
+                <th className="py-8">رمز عبور</th>
+                <th className="py-8">شماره تماس</th>
+                <th className="py-8">ایمیل</th>
+                <th className="py-8">وضعیت</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((item: any) => {
+                return <User key={item.id} {...item} />;
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <>{!isLoading && <ErrorBox message="هیچ کاربری یافت نشد" />}</>
+      )}
     </>
   );
 };
