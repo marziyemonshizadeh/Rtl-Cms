fa103c2a5f137636745f236d1f29fedb3b51bb1c
marzieh <marziehmonshizade@yahoo.com>
2023-11-07 17:48:08 +0330

    refactor: refactor orders  page

--- src/pages/orders/orders.tsx
+++ src/pages/orders/orders.tsx
@@ -1,50 +1,41 @@
+import { useQuery } from "react-query";
 import ErrorBox from "../../components/errorBox/errorBox";
+import Order from "../../components/order/order";
 
 const Orders: React.FC = () => {
+  const { data, isLoading } = useQuery("orders", () =>
+    fetch("http://localhost:3001/orders").then((res) => res.json())
+  );
   return (
     <>
       <h1 className="header"> لیست سفارشات ثبت شده </h1>
-      <ErrorBox message="هیچ سفارشی یافت نشد" />
-
-      <div className="card mt-5">
-        <table className="w-full">
-          <thead className="border-b-2">
-            <tr>
-              <th className="py-8">نام محصول خریداری شده</th>
-              <th className="py-8">نام خریدار</th>
-              <th className="py-8">تاریخ سفارش</th>
-              <th className="py-8">ساعت سفارش</th>
-              <th className="py-8">مبلغ کل</th>
-              <th className="py-8"> تخفیف اعمال شده</th>
-              <th className="py-8">وضعیت</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="text-center py-8">لپ تاپ msi</td>
-              <td className="text-center py-8">مرضیه منشی زاده </td>
-              <td className="text-center py-8">13 شهریور 1402 </td>
-              <td className="text-center py-8">16:30</td>
-              <td className="text-center py-8">35,000,000</td>
-              <td className="text-center py-8">0%</td>
-              <td className="py-8 text-center">
-                <button type="button" className="btn">
-                  جزئیات
-                </button>
-                <button type="button" className="btn">
-                  حذف
-                </button>
-                <button type="button" className="btn">
-                  بعدا بررسی می شود
-                </button>
-                <button type="button" className="btn">
-                  تایید
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {isLoading && (
+        <div className="mx-auto text-gray-500">لطفا کمی صبر کنید...</div>
+      )}
+      {data?.length ? (
+        <div className="card mt-5">
+          <table className="w-full">
+            <thead className="border-b-2">
+              <tr>
+                <th className="py-8">نام محصول خریداری شده</th>
+                <th className="py-8">نام خریدار</th>
+                <th className="py-8">تاریخ سفارش</th>
+                <th className="py-8">ساعت سفارش</th>
+                <th className="py-8">مبلغ کل</th>
+                <th className="py-8"> تخفیف اعمال شده</th>
+                <th className="py-8">وضعیت</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((item: any) => {
+                return <Order key={item.id} {...item} />;
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <>{!isLoading && <ErrorBox message="هیچ سفارشی یافت نشد" />}</>
+      )}
     </>
   );
 };
