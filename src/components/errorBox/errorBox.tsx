57545ca8ac94bbfc1cb5a5d43e9e532c5e72bbc3
marzieh <marziehmonshizade@yahoo.com>
2023-11-10 01:19:54 +0330

    add: add dark class

--- src/components/errorBox/errorBox.tsx
+++ src/components/errorBox/errorBox.tsx
@@ -3,7 +3,7 @@ interface errorBoxProps {
 }
 export default function errorBox({ message }: errorBoxProps) {
   return (
-    <div className="w-full bg-red-950 text-gray-50 text-center font-bold p-3">
+    <div className="w-full bg-red-950 dark:bg-gray-500  text-gray-50 text-center font-bold p-3">
       {message}
     </div>
   );
