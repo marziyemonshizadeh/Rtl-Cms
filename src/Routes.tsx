82aabb59a85a8d7d141136f8f6f8fe8d868af367
marzieh <marziehmonshizade@yahoo.com>
2023-11-09 17:59:14 +0330

    add: add homepage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Comments from "./pages/comments/comments";
 import Discounts from "./pages/discounts/discounts";
+import Home from "./pages/home/home";
 import Orders from "./pages/orders/orders";
 import Products from "./pages/products/products";
 import Users from "./pages/users/users";
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
+      { path: "/", element: <Home /> },
       { path: "comments", element: <Comments /> },
       { path: "discount", element: <Discounts /> },
       { path: "orders", element: <Orders /> },
