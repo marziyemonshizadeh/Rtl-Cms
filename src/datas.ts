28f4c6c9f3032a95e4c4c3c87a956660131e0113
marzieh <marziehmonshizade@yahoo.com>
2023-11-11 21:45:40 +0330

    add: Add datas.ts for charts

---
+++ src/datas.ts
@@ -0,0 +1,73 @@
+let xAxisData = [
+    {
+      month: "فروردین",
+      Sale: 112_000,
+      expenditure: 100_000,
+    },
+    {
+      month: "اردیبهشت",
+      Sale: 99_000,
+      expenditure: 100_000,
+    },
+    {
+      month: "خرداد",
+      Sale: 12_080,
+      expenditure: 10_000,
+    },
+    {
+      month: "تیر",
+      Sale: 54_000,
+      expenditure: 0,
+    },
+    {
+      month: "مرداد",
+      Sale: 85_000,
+      expenditure: 150_000,
+    },
+    {
+      month: "شهریور",
+      Sale: 120_000,
+      expenditure: 10_000,
+    },
+    {
+      month: "مهر",
+      Sale: 85_000,
+      expenditure: 50_000,
+    },
+    {
+      month: "آبان",
+      Sale: 34_000,
+      expenditure: 36_500,
+    },
+    {
+      month: "آذر",
+      Sale: 18_595,
+      expenditure: 4_000,
+    },
+    {
+      month: "دی",
+      Sale: 73_078,
+      expenditure: 3_078,
+    },
+    {
+      month: "بهمن",
+      Sale: 12_900,
+      expenditure: 200_000,
+    },
+    {
+      month: "اسفند",
+      Sale: 97_000,
+      expenditure: 7_000,
+    },
+  ];
+  const AnnualSalesOfEachProduct = [
+    { name: "Macbook", count: 7, fill: "#82CA9D" },
+    { name: "Asus", count: 3, fill: "#8DD1E1" },
+    { name: "Dell", count: 15, fill: "#FFC658" },
+    { name: "msi", count: 50, fill: "#A4DE6C" },
+  ];
+  export {
+  AnnualSalesOfEachProduct,
+  xAxisData
+};
+  
