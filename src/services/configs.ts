86556898d178b81c7ee2164491539bb36c8a55bd
marzieh <marziehmonshizade@yahoo.com>
2023-11-16 03:21:07 +0330

    change base url

--- src/services/configs.ts
+++ src/services/configs.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //baseurl
 const apiRequests = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
     Auth: "Bearer Token",
