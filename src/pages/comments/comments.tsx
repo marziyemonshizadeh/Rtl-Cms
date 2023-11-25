1f408be14a72a519320717481dea8a4cfb10776b
marzieh <marziehmonshizade@yahoo.com>
2023-11-25 10:04:11 +0330

    fetch comments

--- src/pages/comments/comments.tsx
+++ src/pages/comments/comments.tsx
@@ -1,15 +1,50 @@
-import { useQuery } from "react-query";
+import { useEffect } from "react";
+// import { useQuery } from "react-query";
+import { useDispatch } from "react-redux";
 import Comment from "../../components/comment/comment";
 import ErrorBox from "../../components/errorBox/errorBox";
+import { useAppSelector } from "../../redux/store";
+import { fetchComment } from "../../redux/store/comments";
 
 const Comments: React.FC = () => {
-  const { data, isLoading } = useQuery("comments", () =>
-    fetch("http://localhost:3001/comments").then((res) => res.json())
-  );
+  // const { data, isLoading } = useQuery("comments", () =>
+  //   fetch("http://localhost:3001/comments").then((res) => res.json())
+  // );
+  const dispatch = useDispatch<any>();
+  const data = useAppSelector((state) => state.comments);
+  console.log("data===", data);
+
+  useEffect(() => {
+    dispatch(fetchComment("comments"));
+  }, []);
   return (
     <>
       <h1 className="header"> کامنت های محصولات</h1>
-      {isLoading && (
+      {data.comments?.length ? (
+        <div className="card mt-5">
+          <table className="w-full">
+            <thead className="border-b">
+              <tr>
+                <th className="py-8">اسم کاربر</th>
+                <th className="py-8">محصول</th>
+                <th className="py-8">کامنت</th>
+                <th className="py-8">تاریخ</th>
+                <th className="py-8">ساعت</th>
+                <th className="py-8">وضعیت</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.comments?.map((item: any) => {
+                return <Comment key={item.id} {...item} />;
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <ErrorBox message="هیچ کامنتی یافت نشد" />
+      )}
+
+      {/* {isLoading && (
         <div className="mx-auto text-gray-500">لطفا کمی صبر کنید...</div>
       )}
       {data?.length ? (
@@ -34,7 +69,7 @@ const Comments: React.FC = () => {
         </div>
       ) : (
         <>{!isLoading && <ErrorBox message="هیچ کامنتی یافت نشد" />}</>
-      )}
+      )} */}
     </>
   );
 };
