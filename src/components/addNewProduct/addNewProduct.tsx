2732046d695d283d6882828da1a0d7790c29a374
marzieh <marziehmonshizade@yahoo.com>
2023-11-28 23:13:14 +0330

    coding input's error

--- src/components/addNewProduct/addNewProduct.tsx
+++ src/components/addNewProduct/addNewProduct.tsx
@@ -44,39 +44,55 @@ const AddNewProduct: React.FC = () => {
       <h1 className="header">افزودن محصول جدید</h1>
       <form className="card min-h-64 pt-5" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid xl:grid-cols-2 md:grid-cols-1 gap-2">
-          <input
-            // defaultValue="test"
-            className="input"
-            type="text"
-            placeholder="اسم محصول خود را بنویسید  "
-            aria-label="name"
-            {...register("name")}
-          />
-          {errors.name && <span>This field is required</span>}
-          <input
-            className="input"
-            type="text"
-            placeholder="قیمت محصول خود را بنویسید  "
-            aria-label="price"
-            {...register("price")}
-          />
-          {errors.img && <span>This field is required</span>}
-          <input
-            className="input"
-            type="text"
-            placeholder="موجودی محصول خود را بنویسید  "
-            aria-label="count"
-            {...register("count")}
-          />
-          {errors.price && <span>This field is required</span>}
-          <input
-            className="input"
-            type="text"
-            placeholder="آدرس عکس محصول خود را بنویسید "
-            aria-label="img"
-            {...register("img")}
-          />
-          {errors.count && <span>This field is required</span>}
+          <div className="flex flex-col">
+            <input
+              // defaultValue="test"
+              className="input"
+              type="text"
+              placeholder="اسم محصول خود را بنویسید  "
+              aria-label="name"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              className="input"
+              type="text"
+              placeholder="قیمت محصول خود را بنویسید  "
+              aria-label="price"
+              {...register("price", { required: true })}
+            />
+            {errors.img && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              className="input"
+              type="text"
+              placeholder="موجودی محصول خود را بنویسید  "
+              aria-label="count"
+              {...register("count", { required: true })}
+            />
+            {errors.price && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <input
+              className="input"
+              type="text"
+              placeholder="آدرس عکس محصول خود را بنویسید "
+              aria-label="img"
+              {...register("img", { required: true })}
+            />
+            {errors.count && (
+              <span className="text-red-500">This field is required</span>
+            )}
+          </div>
         </div>
         <div className="flex justify-end mt-3">
           <button
