47323667b588789658663e1c880425087e1f0e21
marzieh <marziehmonshizade@yahoo.com>
2023-12-03 02:49:50 +0330

    Set the dark mode style for modals

--- src/components/modalsTemplate/modalsTemplate.tsx
+++ src/components/modalsTemplate/modalsTemplate.tsx
@@ -14,7 +14,7 @@ export default function ModalsTemplate({ children }: ModalsTemplateProps) {
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 dark:text-black">
               {children}
             </div>
           </div>
